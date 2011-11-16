406696736cc20ecea900493e08bc01862034b345
Chad Eubanks <ceubanks@thecodeboutique.com>
2011-11-15 18:54:09 -0800

    added the desktop view with all app icons.

--- apps/endeavour/states/loginState.js
+++ apps/endeavour/states/loginState.js
@@ -7,12 +7,12 @@ Endeavour.LoginState = SC.State.extend({
       enterState: function() {
         console.log('enterState: loadLoginState');
         Endeavour.getPath('mainPage.mainPane').append();
-        this.invokeLater(this.fadeInTexture, 50);
+        this.invokeLater(this.fadeInTexture, 500);
       },
       
       fadeInTexture: function() {
-        Endeavour.mainPage.mainPane.linenView.animate('opacity', .8,{duration: 1.0,timing:'linear'});
-        Endeavour.mainPage.mainPane.contentWrapper.animate('opacity', 1.0,{duration: 1.0,timing:'linear'});        
+        Endeavour.mainPage.mainPane.linenView.animate('opacity', .8,{duration: 0.8,timing:'linear'});
+        Endeavour.mainPage.mainPane.contentWrapper.animate('opacity', 1.0,{duration: 0.8,timing:'linear'});        
       },
       
       goToDesktopState: function() {
--- apps/endeavour/views/desktop_view.js
+++ apps/endeavour/views/desktop_view.js
@@ -16,6 +16,57 @@ Endeavour.desktopView = EOS.Page.design({
     linenView: EOS.LinenView.design({
       layout: { top: 0, right: 0, bottom: 0, left: 0 },
       classNames: 'linen_eighty_opacity'.w(),
+      childViews: "newsApp designerApp cloudApp rolodexApp appShopApp browserApp accountApp".w(),
+      
+      newsApp: EOS.ButtonView.design({
+        classNames: 'news'.w(),
+        layout: { top: 55, left: 10, height: 114, width: 130},
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      designerApp: EOS.ButtonView.design({
+        classNames: 'designer'.w(),
+        layout: { top: 55, left: 150, height: 114, width: 130},
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      cloudApp: EOS.ButtonView.design({
+        classNames: 'cloud'.w(),
+        layout: { top: 55, left: 290, height: 114, width: 130},
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      rolodexApp: EOS.ButtonView.design({
+        classNames: 'rolodex'.w(),
+        layout: { top: 55, left: 430, height: 114, width: 130},
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      appShopApp: EOS.ButtonView.design({
+        classNames: 'app_shop'.w(),
+        layout: { top: 55, left: 570, height: 114, width: 130},
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      browserApp: EOS.ButtonView.design({
+        layout: { top: 55, left: 710, height: 114, width: 130},
+        classNames: 'browser'.w(),
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
+      accountApp: EOS.ButtonView.design({
+        layout: { top: 55, left: 850, height: 114, width: 130},
+        classNames: 'account'.w(),
+        // action: 'myMethod',
+        // target: 'MyApp.Controller'
+      }),
+      
     })
       
   })
