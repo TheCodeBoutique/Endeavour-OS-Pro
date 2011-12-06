2cd9c89abcba2564a141fbcdbfc069f5525ca52f
Chad Eubanks <ceubanks@thecodeboutique.com>
2011-12-05 23:13:20 -0800

    added the interfcae button, storyboard button, and run button.

--- apps/endeavour/views/designer_app/designer_app_mobile_storyboard.js
+++ apps/endeavour/views/designer_app/designer_app_mobile_storyboard.js
@@ -16,12 +16,19 @@ Endeavour.designerAppMobileStoryboard = EOS.Page.design({
     topBar: EOS.ToolbarView.design({
        layout: { top: 0, left: 0, right: 0, height: 61 },
        classNames: 'EOS_Toolbar_Top_Dark_Shadow'.w(),
-       childViews: ["designerIcon"],
+       childViews: ["designerIcon", "runButton"],
        
        designerIcon: EOS.ImageView.design ({
          layout: { top: 0, left: 10, height: 63, width: 300 },
          useCanvas: YES,
          value: sc_static('images/designer_icon.png')
+       }),
+       
+       runButton: EOS.ButtonView.design({
+         classNames: 'run_button'.w(),
+         layout: { centerY: -2, right: 250 , height: 54, width: 94},
+         // action: 'myMethod',
+         // target: 'Endeavour.statechart'
        }),
        
     }),
@@ -32,8 +39,24 @@ Endeavour.designerAppMobileStoryboard = EOS.Page.design({
     }),
     
     bottomBar: EOS.ToolbarView.design({
-       layout: { bottom: 0, left: 0, right: 0, height: 50 },
+       layout: { bottom: 0, left: 0, right: 0, height: 55 },
        classNames: 'EOS_Toolbar_Bottom_Shadow'.w(),
+       childViews: ["storyboardButton", "interfaceButton"],
+       
+       storyboardButton: EOS.ButtonView.design({
+         classNames: 'storyboard_button'.w(),
+         layout: { centerY: -2, left: 10 , height: 54, width: 94},
+         // action: 'myMethod',
+         // target: 'Endeavour.statechart'
+       }),
+       
+       interfaceButton: EOS.ButtonView.design({
+          classNames: 'interface_button'.w(),
+          layout: { centerY: 0, left: 120 , height: 50, width: 70},
+          // action: 'myMethod',
+          // target: 'Endeavour.statechart'
+        }),
+       
     })
     
   }),
